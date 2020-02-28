f84991198ed5362fe3a126f08e9e0d38c6ef8836
Matti Nilsson <matti.nilsson980105@gmail.com>
2020-02-28 09:04:46 +0100

    fixed game rules

--- src/Components/Grid.js
+++ src/Components/Grid.js
@@ -11,8 +11,8 @@ const Container = styled.aside`
 }
 .gameBoard{
   display:flex;
-  width: 80%;
-  height: 80%;
+  width: 55%;
+  height: ${window.innerHeight / 1.2}px;
   justify-content: space-evenly;
   align-items: space-evenly;
 }
@@ -20,20 +20,75 @@ const Container = styled.aside`
 .column{
   display:flex;
   flex-direction: column;
+  margin-left: 10px;
+  margin-right : 10px;
+  flex-grow: 1;
 }
 
 .card{
-  border: 2px solid black;
+  border: 2px solid white;
   border-radius: 20px;
   margin-top: 20px;
-  width: 100px;
-  height: 150px;
+  transition: .5 linear all;
+  flex-grow: 1;
+}
+.visable{
+  border : 2px solid white;
+  transition: .5 linear all;
+}
+.hidden:hover{
+  border: 2px solid blue;
+  transition: .5 linear all;
 }
 `
 
 function Grid(){
-  const [grid, changeGrid] = useState(makeGrid("normal"))
+  const [grid, setGrid] = useState(makeGrid("normal"))
+  const [check, setCheck] = useState(0)
+  const [pair, setPair] = useState([]);
   console.log(grid);
+
+  function showCard(grid, i, j){
+    let newGrid = [...grid];
+    let newPair = [...pair];
+    newGrid[i][j].visable = (!newGrid[i][j].visable);
+    newPair[check] = newGrid[i][j];
+    setPair(newPair)
+    setCheck(check + 1);
+    setGrid(newGrid);
+  }
+  console.log(pair)
+  if(check === 2){
+    if(pair[0].color === pair[1].color){
+      console.log("FOUND PAIR")
+      console.log(pair[0])
+      let newGrid = [...grid];
+      newGrid[pair[0].x][pair[0].y].cleared = true;
+      newGrid[pair[1].x][pair[1].y].cleared = true;
+      console.log(newGrid[pair[0].x][pair[0].y])
+      setGrid(newGrid);
+      setCheck(0);
+      setPair([]);
+    }
+  }
+
+  if(check === 3){
+    console.log("not a pair..")
+      let newGrid = [...grid];
+      for(let i = 0; i < newGrid.length; i++){
+        for(let j = 0; j < newGrid[i].length; j++){
+          if(newGrid[i][j].cleared === false){
+            newGrid[i][j].visable = false;
+          }
+        }
+      }
+      newGrid[pair[2].x][pair[2].y].visable = true;
+    setGrid(newGrid);
+    setCheck(1);
+    setPair([pair[2]]);
+  }
+
+
   return (
     <Container>
       <div className="overAll">
@@ -43,7 +98,14 @@ function Grid(){
             return(
               <div key={columnID} className="column">
                 {column.map((card, cardID) =>{
-                  return(<div key={cardID} className="card" style={{backgroundColor : card.color}}></div>)
+                  return(<div 
+                    key={cardID} 
+                    className={"card " + (card.visable ? "visable" : "hidden")} 
+                    style={{backgroundColor : card.visable ? card.color : "black"}}
+                    onClick={(e) => showCard(grid, columnID, cardID)}
+                    >
+                      
+                    </div>)
                 })}
               </div>
             )
--- src/utilities/makeGrid.js
+++ src/utilities/makeGrid.js
@@ -1,5 +1,5 @@
 export const makeGrid = (diff) =>{
-  let colors = randomizeColors();
+  let colors = randomizeColors(diff);
   let grid = [];
   let whatColor = 0;
   if(diff === "normal"){
@@ -10,6 +10,38 @@ export const makeGrid = (diff) =>{
           visable : false,
           cleared : false,
           color : colors[whatColor],
+          x : i,
+          y : j,
+        }
+        whatColor++;
+      }
+    }
+  }
+  if(diff === "easy"){
+    for(let i = 0; i < 4; i++){
+      grid[i] = [];
+      for(let j = 0; j < 3; j++){
+        grid[i][j] = {
+          visable : false,
+          cleared : false,
+          color : colors[whatColor],
+          x : i,
+          y : j,
+        }
+        whatColor++;
+      }
+    }
+  }
+  if(diff === "baby"){
+    for(let i = 0; i < 4; i++){
+      grid[i] = [];
+      for(let j = 0; j < 2; j++){
+        grid[i][j] = {
+          visable : false,
+          cleared : false,
+          color : colors[whatColor],
+          x : i,
+          y : j,
         }
         whatColor++;
       }
@@ -18,30 +50,72 @@ export const makeGrid = (diff) =>{
   return grid;
 }
 
-function randomizeColors(){
-  let randomColors = [];
-  let colors = [
-    {color : "gray", amount : 2},
-    {color : "blue", amount : 2},
-    {color : "red", amount : 2},
-    {color : "purple", amount : 2},
-    {color : "pink", amount : 2},
-    {color : "yellow", amount : 2},
-    {color : "black", amount : 2},
-    {color : "white", amount : 2},
-    {color : "cyan", amount : 2},
-  ]
-  for(let i = 0; i < 18; i++){
-    let randomNumber = Math.floor(Math.random() * Math.floor(colors.length));
-    if(colors[randomNumber].amount > 0){
-      colors[randomNumber].amount -= 1;
-      randomColors[i] = colors[randomNumber].color;
+function randomizeColors(diff){
+  if(diff === "normal"){
+    let randomColors = [];
+    let colors = [
+      {color : "gray", amount : 2},
+      {color : "blue", amount : 2},
+      {color : "red", amount : 2},
+      {color : "purple", amount : 2},
+      {color : "pink", amount : 2},
+      {color : "yellow", amount : 2},
+      {color : "orange", amount : 2},
+      {color : "brown", amount : 2},
+      {color : "cyan", amount : 2},
+    ]
+    for(let i = 0; i < 18; i++){
+      let randomNumber = Math.floor(Math.random() * Math.floor(colors.length));
+      if(colors[randomNumber].amount > 0){
+        colors[randomNumber].amount -= 1;
+        randomColors[i] = colors[randomNumber].color;
+      }
+      if(colors[randomNumber].amount === 0){
+        colors.splice(randomNumber, 1);
+      }
     }
-    if(colors[randomNumber].amount === 0){
-      colors.splice(randomNumber, 1);
-    }
+    return randomColors;
   }
-  console.log(randomColors);
-  console.log(colors);
-  return randomColors;
+  if(diff === "easy"){
+    let randomColors = [];
+    let colors = [
+      {color : "gray", amount : 2},
+      {color : "blue", amount : 2},
+      {color : "red", amount : 2},
+      {color : "purple", amount : 2},
+      {color : "pink", amount : 2},
+      {color : "yellow", amount : 2},
+    ]
+    for(let i = 0; i < 12; i++){
+      let randomNumber = Math.floor(Math.random() * Math.floor(colors.length));
+      if(colors[randomNumber].amount > 0){
+        colors[randomNumber].amount -= 1;
+        randomColors[i] = colors[randomNumber].color;
+      }
+      if(colors[randomNumber].amount === 0){
+        colors.splice(randomNumber, 1);
+      }
+    }
+    return randomColors;
+  }
+  if(diff === "baby"){
+    let randomColors = [];
+    let colors = [
+      {color : "gray", amount : 2},
+      {color : "blue", amount : 2},
+      {color : "red", amount : 2},
+      {color : "purple", amount : 2},
+    ]
+    for(let i = 0; i < 8; i++){
+      let randomNumber = Math.floor(Math.random() * Math.floor(colors.length));
+      if(colors[randomNumber].amount > 0){
+        colors[randomNumber].amount -= 1;
+        randomColors[i] = colors[randomNumber].color;
+      }
+      if(colors[randomNumber].amount === 0){
+        colors.splice(randomNumber, 1);
+      }
+    }
+    return randomColors;
+  }
 }
